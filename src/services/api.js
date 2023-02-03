08d9ecb65788d44c0a705f158bb2bdc8189a3d7d
PreetinderSingh345 <preetindersingh072@gmail.com>
2023-02-03 13:20:38 +0530

    feat: add some companies to the database

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,38 @@
-const {Company} = require('../../database/models');
+const axios = require('axios');
+
+const {Company} = require('../../database/models/index');
 
 const saveCompanies = async (urlLink) => {
-    const output= await "Please return this";
+    const externalApiResponse = await axios.get(urlLink);
+
+    let companyInfo=[];
+
+    const companyInfoCsv = externalApiResponse.data;
+
+    const companyInfoSplitByLine=companyInfoCsv.split('\n');
+
+    companyInfoSplitByLine.shift();
+
+    companyInfoSplitByLine.forEach(element => {
+        companyInfo.push(element.split(','))
+    });
+
+    companyInfo.forEach(async element => {
+        const companyId = element[0];
+        const companySector = element[1];
+
+        const externalApiRespnonseId = await axios.get(`http://54.167.46.10/company/${companyId}`);
+
+        const {name, ceo} = externalApiRespnonseId.data;
+
+        const company = await Company.create({
+            companyId: companyId,
+            name: name,
+            ceo: ceo,
+            sector: companySector,
+            score: 0
+        });
+    });
 
     return urlLink;
 };
