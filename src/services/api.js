6b1ab83b3b71fc07e80d7a5bbb80b6b2ababbba9
PreetinderSingh345 <preetindersingh072@gmail.com>
2023-02-03 14:13:14 +0530

    feat: implement save company data functionality

--- src/services/api.js
+++ src/services/api.js
@@ -17,9 +17,22 @@ const saveCompanies = async (urlLink) => {
         companyInfo.push(element.split(','))
     });
 
+    let uniqueIds = new Set();
+    let uniqueSectors = new Set();
+
     companyInfo.forEach(async element => {
         const companyId = element[0];
         const companySector = element[1];
+
+        uniqueIds.add(companyId);
+        uniqueSectors.add(companySector);
+    });
+
+    companyInfo.forEach(async element => {
+        const companyId = element[0];
+        const companySector = element[1];
+
+        uniqueSectors.add(companySector);
 
         const externalApiRespnonseId = await axios.get(`http://54.167.46.10/company/${companyId}`);
 
@@ -34,7 +47,34 @@ const saveCompanies = async (urlLink) => {
         });
     });
 
-    return urlLink;
+    uniqueSectors.forEach(async sector => {
+        const externalApiResponseSector = await axios.get(`http://54.167.46.10/sector?name=${sector}`);
+
+        const companyInfoSector = externalApiResponseSector.data;
+
+        companyInfoSector.forEach(async element => {
+            const {companyId, performanceIndex} = element;
+
+            if(uniqueIds.has(companyId)) {
+                const cpi = performanceIndex[0]['value'];
+                const cf = performanceIndex[1]['value'];
+                const mau = performanceIndex[2]['value'];
+                const roic = performanceIndex[3]['value'];
+
+                const score = ((cpi * 10) + (cf / 10000) + (mau * 10) + roic) / 4;
+
+                await Company.update({
+                    score: score
+                }, {
+                    where: {
+                        companyId: companyId
+                    }
+                });
+            }
+        });
+    });
+
+    return 'Companies saved successfully';
 };
 
 module.exports = {saveCompanies};
